fe9ba707c3e51f14d575b8634951d208af0e125a
byq20040220 <2630174726@qq.com>
2024-05-19 21:05:40 +0800

    静态资源配置

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,6 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#1导入系统的logging
+import logging
+#2创建日志器
+logger = logging.getLogger('django')
+
+from django.http import HttpResponse
+def log(request):
+    logger.info('info')
+    return HttpResponse('test')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',log),
 ]
